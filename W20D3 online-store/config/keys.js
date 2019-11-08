4adb95eadb240c12b667cba562168b3020f894b1
FridaPolished <fridapulido@gmail.com>
2019-11-07 16:33:32 -0800

    Write root query type for products to add random price

--- W20D3 online-store/config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  MONGO_URI: "mongodb+srv://devs:3QeYTpQOFSGGkdbu@greekgods-xrh7c.mongodb.net/onlinestore?retryWrites=true&w=majority",
-  secretOrKey: "e1619894-d35b-4ded-b1e4-624bc9e15d0c",
-  AWSKey: "c6C9dt3jDW7J3NYT4GP0r1uT1Bon9LDh7Bpdz8lu"
-};
